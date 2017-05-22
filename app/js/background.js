2e5845f7f4685e126e7762a268689f1a307cb7aa
moti <moti@practitest.com>
2017-05-22 10:40:38 +0700

    encrypt options
    store keys in idexedDB
    TODO: change iv every encryption

--- app/js/background.js
+++ app/js/background.js
@@ -33,3 +33,30 @@ function fetchStatus(options){
     console.log(data)
   })
 }
+
+
+let optionsTabId = ""
+
+function openOptionsTab() {
+  if(optionsTabId){
+    chrome.tabs.get(optionsTabId, (tab) => {
+      if(tab) chrome.tabs.update(tab.id, {active: true})
+      chrome.windows.getCurrent({}, (currentWindow) => {
+        if(tab.windowId != currentWindow.id){
+          chrome.windows.update(tab.windowId, {focused: true})
+        }
+      })
+    })
+  }
+  else{
+    chrome.tabs.create({'url': chrome.extension.getURL('options.html'),
+      'selected': true}, (tab) => optionsTabId = tab.id);
+  }
+}
+
+chrome.browserAction.onClicked.addListener(openOptionsTab)
+
+chrome.tabs.onRemoved.addListener((tabId, changeInfo, tab) => {
+  if(optionsTabId == tabId)
+    optionsTabId = ""
+})
--- app/js/options.js
+++ app/js/options.js
@@ -1,29 +1,105 @@
+function generateKey(){
+  var usages = ['encrypt', 'decrypt']
+  var extractable = false
+  return crypto.subtle.generateKey({ name: 'AES-GCM', length: 256 },
+    extractable,
+    usages)
+}
+
+function getKeyIv(){
+  return new Promise(function(resolve,reject){
+    var db = new Dexie("KeyStore")
+    db.version(1).stores({
+      keys: 'name,key,iv'
+    })
+    db.keys.get('MyKey').then(function(keyFromDB){
+      if(keyFromDB){
+        resolve(keyFromDB)
+      }else{
+        generateKey().then(function(generatedKey){
+          const iv = crypto.getRandomValues(new Uint8Array(12))
+          db.keys.put({name: "MyKey", key: generatedKey, iv: iv}).then(function(){
+            resolve({key:generatedKey, iv: iv})
+          })
+        })
+      }
+    })
+  })
+}
+
+const strToUInt8 = str => Uint8Array.from(str.split(''), c=>c.charCodeAt(0))
+const uInt8ToStr = buf => String.fromCharCode(...buf)
+
+const loadUInt8 = str => Uint8Array.from(str.split(','))
+const storeUInt8 = buf => buf.toString()
+
+function loadOptions(){
+  return new Promise(function(resolve, reject){
+    chrome.storage.local.get('enc_options', ({enc_options}) => {
+      getKeyIv().then(function({key, iv}){
+        const buffer = loadUInt8(enc_options);
+        return crypto.subtle.decrypt({name: "AES-GCM",iv:iv}, key, buffer)
+        .then(function(decrypted){
+          let decryptedArr = new Uint8Array(decrypted)
+          let decodedStr = uInt8ToStr(decryptedArr)
+          const optionsObj = JSON.parse(decodedStr)
+          resolve(optionsObj)
+        })
+      }).catch((err)=>{
+        console.error(err)
+      })
+    })
+  })
+}
+
+function saveOptions(options){
+  return new Promise(function(resolve, reject){
+    const stringified = JSON.stringify(options)
+    getKeyIv().then(function({key,iv}){
+      const buffer = strToUInt8(stringified);
+      crypto.subtle.encrypt({
+        name: "AES-GCM",
+        iv: iv
+      }, key, buffer).then(function(encrypted){
+        let encArr = new Uint8Array(encrypted)
+        const decodedStrOptions = storeUInt8(encArr)
+        chrome.storage.local.set({'enc_options':decodedStrOptions},(res) => {
+          if(chrome.runtime.lastError){
+            reject(runtime.lastError)
+          }
+          resolve()})
+      })
+    })
+  })
+}
+
 const $i = document.getElementById.bind(document)
 
-let defaultOptions = {regionName:'',keyId:'',accessKey:'',instanceId:'',refreshRate:''}
+const defaultOptions = {regionName:'',keyId:'',accessKey:'',instanceId:'',refreshRate:''}
 
-chrome.storage.sync.get('options', ({options = defaultOptions}) => {
+loadOptions().then((options = defaultOptions) => {
   Reflect.ownKeys(options).forEach(function(key){
     $i(key).value = options[key]
   })
   $i('save_btn').classList.remove('disabled')
+}).catch((err)=>{
+  console.error(err)
 })
 
 $i('save_btn').addEventListener('click',save)
 
 function save(event){
-  let options = defaultOptions
-  Reflect.ownKeys(options).forEach(function(key){
-    options[key] = $i(key).value.trim()
-  })
+  const options = Reflect.ownKeys(defaultOptions).reduce(function(acc, val){
+    acc[val] = $i(val).value.trim()
+    return acc
+  },{})
   if(isValid()){
-    chrome.storage.sync.set({'options':options})
-    window.close()
+    saveOptions(options).then(() => console.info(0))
   }else{
     const targetClassList = event.target.classList
     if(targetClassList.contains('shake-btn')){
       targetClassList.remove('shake-btn')
-      void event.target.offsetWidth // hack to restart animation (forces reflow)
+      void event.target.offsetWidth // restart animation (forces reflow)
     }
     targetClassList.add('shake-btn')
     event.preventDefault()
